42cbc6c0f922746f0f8bfa90a6276ca2088a50f6
Hans Hörberg <hans.horberg@umu.se>
2015-11-04 11:03:09 +0100

    Added internal attributes configuration.

--- src/vopaas/backends/oidc_backend.py
+++ src/vopaas/backends/oidc_backend.py
@@ -5,9 +5,9 @@ __author__ = 'mathiashedstrom'
 
 
 class VOPaaSOpenIdBackend(OpenIdBackend, VOPaaSBackendModule):
-    def __init__(self, auth_callback_func, config):
+    def __init__(self, auth_callback_func, internal_attributes, config):
         self.oidc_backend_config = config
-        super(VOPaaSOpenIdBackend, self).__init__(auth_callback_func, config)
+        super(VOPaaSOpenIdBackend, self).__init__(auth_callback_func,internal_attributes, config)
 
     def get_metadata_desc(self):
         return get_metadata_desc_for_oidc_backend(self.oidc_backend_config)
