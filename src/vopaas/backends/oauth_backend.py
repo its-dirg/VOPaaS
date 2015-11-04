0c9db0ae99b2bf309d947840b36e50d8e23bc8f8
Hans Hörberg <hans.horberg@umu.se>
2015-11-04 12:58:23 +0100

    Added a OAuth backend.

---
+++ src/vopaas/backends/oauth_backend.py
@@ -0,0 +1,13 @@
+from satosa.backends.oauth import OAuthBackend
+from vopaas.backends.backend_base import VOPaaSBackendModule, get_metadata_desc_for_oidc_backend
+
+__author__ = 'mathiashedstrom'
+
+
+class VOPaaSOAuthBackend(OAuthBackend, VOPaaSBackendModule):
+    def __init__(self, auth_callback_func, internal_attributes, config):
+        self.oidc_backend_config = config
+        super(VOPaaSOAuthBackend, self).__init__(auth_callback_func,internal_attributes, config)
+
+    def get_metadata_desc(self):
+        return get_metadata_desc_for_oidc_backend(self.oidc_backend_config)
