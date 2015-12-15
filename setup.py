9f779034f902e2151128bddf2f14b22f58bcc6ff
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-12-15 15:26:45 +0100

    Updated vopaas to use new SATOSA

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from setuptools import setup
 setup(
     name='VOPaaS',
     version='',
-    packages=['vopaas', 'vopaas.backends', 'vopaas.frontends', 'vopaas.metadata_creation'],
+    packages=['vopaas'],
     package_dir={'': 'src'},
-    scripts=["src/vopaas/metadata_creation/make_vopaas_metadata.py"],
     url='',
     license='',
     author='DIRG',
     author_email='',
-    description='',
-    install_requires=["pluginbase"],
+    description=''
 )
--- src/vopaas/backends/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Python package file
-"""
-
