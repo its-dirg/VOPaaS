b6fad39eb960071c02802468a4e435672232423b
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-12-18 09:54:45 +0100

    Use proper version of SATOSA (from PyPI).

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='VOPaaS',
-    version='',
-    packages=['vopaas'],
+    version='0.1',
+    packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='',
-    license='',
+    url='https://github.com/its-dirg/VOPaaS',
+    license='Apache License 2.0',
     author='DIRG',
-    author_email='',
-    description=''
+    author_email='dirg@its.umu.se',
+    description='Protocol proxy for the Virtual Organization Platform as a Service (VOPaaS).',
+    install_requires=[
+        'satosa==0.4.1'
+    ]
 )
